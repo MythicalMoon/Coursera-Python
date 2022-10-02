59d4fa3075a078eec034f7b35e34d6182bc0665b
MythicalMoon <KasparsSB@hotmail.com>
2022-10-01 23:30:51 -0400

    Week14

---
+++ Week14/ex1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+#Exercise 1: Change the socket program socket1.py to prompt the user
+#for the URL so it can read any web page. You can use split('/') to
+#break the URL into its component parts so you can extract the host
+#name for the socket connect call. Add error checking using try and
+#except to handle the condition where the user enters an improperly
+#formatted or non-existent URL.
+
+import socket
+import requests
+import urllib.request, urllib.error, urllib.parse
+
+url = input('URL: ')
+
+try:
+    requests.get(url)
+except requests.RequestException:
+    print("Improper URL!")
+    exit()
+
+hostname = url.split('/')[2]
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((hostname, 80))
+mysock.send(('GET '+url+' HTTP/1.0\r\n\r\n').encode())
+
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
